c7935b4d98bb003cc8d2d540d35f1be0c84348ca
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-16 11:46:04 -0700

    Update activity7.cpp

--- activity7.cpp
+++ activity7.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 int main(){
-  char array[3][20];
+  string array[3];
   cout << "Enter a name: " << endl;
   cin >> array[0];
   cout << "Enter a second name: " << endl;
   cin >> array[1];
   cout << "Enter a third name: " << endl;
   cin >> array[2];
-  for(int i = 0; array[0][i] != '\0'; i++){
-    cout << array[0][i];
-  }
+  cout << array[0];
   return 0;
 }
